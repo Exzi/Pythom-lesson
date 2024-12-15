9c7bc77894aa225c98a6b70e376ff8ad88f0b3b8
Ilya <iliya176@yandex.ru>
2024-12-15 23:53:46 +0300

    Написан код добавления one-hot представления для значения robot

--- Task_44.py
+++ Task_44.py
@@ -7,3 +7,8 @@ random.shuffle(lst)
 data = pd.DataFrame({'who': lst})
 
 print(data.head())
+
+one_hot_robot = pd.DataFrame({'robot': [1 if x == 'robot' else 0 for x in data['who']]})
+result_robot = pd.concat([data, one_hot_robot], axis=1)
+
+print(result_robot.head())
